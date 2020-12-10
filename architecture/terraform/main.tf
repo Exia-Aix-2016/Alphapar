bef1153726f975eda47e3f82b9d9f5c74fe80ae4
Mandel VAUBOURG <mandel.vaubourg@viacesi.fr>
2020-12-10 17:37:58 +0100

    Edit

--- architecture/terraform/main.tf
+++ architecture/terraform/main.tf
@@ -14,16 +14,15 @@ resource "proxmox_lxc" "service-keycloak" {
         storage = "pool"
         size = "16G"
     }
-    lifecycle { 
-     ignore_changes = [
-            network,
-            target_node,
-        ]
-    }
+    # lifecycle { 
+    #  ignore_changes = [
+    #         network,
+    #         target_node,
+    #     ]
+    # }
+
+
     ssh_public_keys = var.sshkey
-
-
-
     memory = 1024
     ostemplate = "cephfs:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
     password = var.pm_password
