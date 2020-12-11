fc1eeaea3f7f51e0f80fc46e4fe6e43199a86522
Mandelv <mandel.vaubourg@viacesi.fr>
2020-12-11 12:39:38 +0100

    Add other services

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,19 @@
+#===========================
+#      SERVICE KEYCLOAK
+#===========================
 resource "proxmox_lxc" "service-keycloak" {
+    ostemplate = "cephfs:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+    hostname = "service-keycloak"
+    ssh_public_keys = var.sshkey
+    password = var.kc_password
+    memory = 1024
+    target_node = "hulk"
+    unprivileged = true
+
     features {
         nesting = true
     }
-    hostname = "service-keycloak"
+
     network {
         name = "eno2"
         bridge = "vmbr0"
@@ -14,19 +25,120 @@ resource "proxmox_lxc" "service-keycloak" {
         storage = "pool"
         size = "16G"
     }
-    # lifecycle { 
-    #  ignore_changes = [
-    #         network,
-    #         target_node,
-    #     ]
-    # }
 
+    lifecycle { 
+     ignore_changes = [
+            network,
+            target_node,
+        ]
+    }
+}
 
+#===========================
+#      SERVICE NEXCLOUD
+#===========================
+resource "proxmox_lxc" "service-nextcloud" {
+    ostemplate = "cephfs:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"   
+    hostname = "service-nextcloud"
+    password = var.nc_password
     ssh_public_keys = var.sshkey
     memory = 1024
-    ostemplate = "cephfs:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
-    password = var.pm_password
-
     target_node = "hulk"
     unprivileged = true
+
+    features {
+        nesting = true
+    }
+    
+    network {
+        name = "eno2"
+        bridge = "vmbr0"
+        ip = "dhcp"
+        ip6 = "dhcp"
+    }
+
+    rootfs {
+        storage = "pool"
+        size = "50G"
+    }
+
+    lifecycle { 
+     ignore_changes = [
+            network,
+            target_node,
+        ]
+    }
+}
+
+#===========================
+#      SERVICE WEB
+#===========================
+resource "proxmox_lxc" "service-web" {
+    ostemplate = "cephfs:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+    hostname = "service-web"
+    target_node = "hulk"
+    password = var.wb_password
+    ssh_public_keys = var.sshkey
+    unprivileged = true 
+    memory = 1024
+    
+    features {
+        nesting = true
+    }
+
+    network {
+        name = "eno2"
+        bridge = "vmbr0"
+        ip = "dhcp"
+        ip6 = "dhcp"
+    }
+
+    rootfs {
+        storage = "pool"
+        size = "50G"
+    }
+
+    lifecycle { 
+     ignore_changes = [
+            network,
+            target_node,
+        ]
+    }
+}
+
+#===========================
+#      SERVICE WEB
+#===========================
+
+resource "proxmox_lxc" "service-database" {
+    ostemplate = "cephfs:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+    hostname = "service-database"
+    target_node = "hulk"
+    password = var.wb_password
+    ssh_public_keys = var.sshkey
+    unprivileged = true 
+    memory = 1024
+    
+    features {
+        nesting = true
+    }
+
+    network {
+        name = "eno2"
+        bridge = "vmbr0"
+        ip = "dhcp"
+        ip6 = "dhcp"
+    }
+
+    rootfs {
+        storage = "pool"
+        size = "50G"
+    }
+
+    lifecycle { 
+     ignore_changes = [
+            network,
+            target_node,
+        ]
+    } 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,11 @@
 
-variable "pm_api_url" {}
+#===========================
+#        PROXMOX
+#===========================
+
+variable "pm_api_url" {
+    sensitive = true
+}
 
 variable "pm_user" {
     sensitive = true
@@ -9,7 +15,42 @@ variable "pm_password" {
     sensitive = true
 }
 
+#===========================
+#      SERVICE SSHKEY
+#===========================
 
 variable "sshkey" {
+    sensitive = true
+}
+
+#===========================
+#      SERVICE KEYCLOAK
+#===========================
+
+variable "kc_password" {
+    sensitive = true
+}
+
+#===========================
+#      SERVICE NEXCLOUD
+#===========================
+
+variable "nc_password" {
+    sensitive = true
+}
+
+#===========================
+#      SERVICE WEB
+#===========================
+
+variable "wb_password" {
+     sensitive = true
+}
+
+#===========================
+#      SERVICE WEB
+#===========================
+
+variable "db_password" {
     sensitive = true
 }
